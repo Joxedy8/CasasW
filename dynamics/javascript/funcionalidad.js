86c3d5215b0e3ddfd71fb462f659c5bc2330dbbd
Joxedy8 <132935758+Joxedy8@users.noreply.github.com>
2023-06-10 19:12:52 -0600

    feat(funcinalidad.js): agrega ver contraseña

--- dynamics/javascript/funcionalidad.js
+++ dynamics/javascript/funcionalidad.js
@@ -1,7 +1,19 @@
 window.addEventListener("click", ()=>{
-    sumar = document.getElementById("sumar");
-    restar = document.getElementById("restar");
-    enviar = document.getElementById("enviar");
+    let verPass = document.getElementById("verPass");
+    let contraseña = document.getElementById("contraseña");
+    let sumar = document.getElementById("sumar");
+    let restar = document.getElementById("restar");
+    let enviar = document.getElementById("enviar");
+    let a = 1;
+    verPass.addEventListener("click", ()=>{
+        if(a==1){
+            contraseña.type = "text";
+            a--;
+        }else{
+            contraseña.type = "password";
+            a++;
+        }
+    })
 
     
     sumar.addEventListener("click", ()=>{
