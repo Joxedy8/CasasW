cd7c5f6019b5a67de097de5e4aa2c53eae3ceae4
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-06-10 21:36:48 -0600

    style: magenes de las casas

--- dynamics/php/selecciona.php
+++ dynamics/php/selecciona.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TUHOME</title>
     <link rel="stylesheet" href="../../statics/styles/style.css">
 
 </head>
@@ -25,16 +25,19 @@
             <section id="halCont" class="homeCW">
                 <article>
                     <h3 id="halT">Halcones</h3>
+                    <img class="imgCW" src="https://cdn0.bioenciclopedia.com/es/posts/3/3/1/halcon_peregrino_133_orig.jpg" alt="halcon">
                 </article>
             </section>
             <section  id="tepoCont" class="homeCW">
                 <article>
                     <h3 id="tepoT" >Teporingos</h3>
+                    <img class="imgCW" src="https://oncenoticias.digital/wp-content/uploads/2021/12/conejo-teporingo-se-extingue-en-nevado-de-toluca.jpg" alt="teporingo">
                 </article>
             </section>
             <section id="ajoCont" class="homeCW">
                 <article>
                     <h3 id="ajoT">Ajolotes</h3>
+                    <img class="imgCW" src="https://www.kebuena.com.mx/wp-content/uploads/2023/02/Di%CC%81a-del-Ajolote_KeBuena.jpg" alt="ajolote">
                 </article>
             </section>
         </span>
